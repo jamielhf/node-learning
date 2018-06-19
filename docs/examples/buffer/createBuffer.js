b6a6082f61c0d281293683be4aabbc27b444fa64
569309786@qq.com <569309786@qq.com>
2018-06-19 22:26:04 +0800

    更新buffer

---
+++ docs/examples/buffer/createBuffer.js
@@ -0,0 +1,14 @@
+// Buffer.from
+
+// Buffer.from(array)： 返回一个被 array 的值初始化的新的 Buffer 实例（传入的 array 的元素只能是数字，不然就会自动被 0 覆盖）
+// Buffer.from(arrayBuffer[, byteOffset[, length]])： 返回一个新建的与给定的 ArrayBuffer 共享同一内存的 Buffer。
+// Buffer.from(buffer)： 复制传入的 Buffer 实例的数据，并返回一个新的 Buffer 实例
+// Buffer.from(string[, encoding])： 返回一个被 string 的值初始化的新的 Buffer 实例
+
+const buf1 = Buffer.from([1,2]);
+
+const buf2 = Buffer.from();
+
+const buf3 = Buffer.from(buf1);
+
+const buf4 = Buffer.from('jamie','utf-8');
--- docs/modules/buffer.md
+++ docs/modules/buffer.md
@@ -1,1 +1,18 @@
 # Buffer
+
+## 模块说明
+
+Buffer 类被引入作为 Node.js API 的一部分，使其可以在 TCP 流或文件系统操作等场景中处理二进制数据流。一个 Buffer 类似于一个整数数组，但它对应于 V8 堆内存之外的一块原始内存
+Node.js 目前支持的字符编码包括：
+
+* ascii - 仅支持 7 位 ASCII 数据。如果设置去掉高位的话，这种编码是非常快的。
+* utf8 - 多字节编码的 Unicode 字符。许多网页和其他文档格式都使用 UTF-8 。
+* utf16le - 2 或 4 个字节，小字节序编码的 Unicode 字符。支持代理对（U+10000 至 U+10FFFF）
+* ucs2 - utf16le 的别名。
+* base64 - Base64 编码。
+* latin1 - 一种把 Buffer 编码成一字节编码的字符串的方式。
+* binary - latin1 的别名。
+* hex - 将每个字节编码为两个十六进制字符。
+
+## 创建buffer
+
